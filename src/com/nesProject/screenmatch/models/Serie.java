3354ecd541d2ec661634c0d6eb3559584594c69b
Nestor-DS <nestorduhamel18@outlook.es>
2024-04-05 22:00:21 -0600

    serie class

---
+++ src/com/nesProject/screenmatch/models/Serie.java
@@ -0,0 +1,31 @@
+package com.nesProject.screenmatch.models;
+
+public class Serie extends Title{
+    int seasons;
+    int episodesSeason;
+    int minutesEpisode;
+
+    public int getSeasons() {
+        return seasons;
+    }
+
+    public void setSeasons(int seasons) {
+        this.seasons = seasons;
+    }
+
+    public int getEpisodesSeason() {
+        return episodesSeason;
+    }
+
+    public void setEpisodesSeason(int episodesSeason) {
+        this.episodesSeason = episodesSeason;
+    }
+
+    public int getMinutesEpisode() {
+        return minutesEpisode;
+    }
+
+    public void setMinutesEpisode(int minutesEpisode) {
+        this.minutesEpisode = minutesEpisode;
+    }
+}
